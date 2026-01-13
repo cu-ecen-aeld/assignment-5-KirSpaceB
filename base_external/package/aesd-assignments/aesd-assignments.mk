736367440c11b03937c2a9e56d7a79018852773c
KirSpaceB <productivekirk@gmail.com>
2026-01-12 17:13:52 -0800

    build install init script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -16,10 +16,21 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	# build writer (assignment 4 stuff)
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app writer
+
+	# build aesdsocket (assignment 5 part 2 stuff)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server clean
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	# aesdsocket binary
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+
+	# init script to start at boot
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 	# writer binary
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 
@@ -31,6 +42,5 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 endef
-
 
 $(eval $(generic-package))
